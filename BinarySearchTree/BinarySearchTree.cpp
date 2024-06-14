118ddd309f1e8822f99f3fb8ec5f360be3d2dee3
Ekoramadhan13 <enugraha411@gmail.com>
2024-06-14 09:44:18 +0700

    menambahkan class Node

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -2,3 +2,23 @@
 #include <string>
 using namespace std;
 
+
+class Node {
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	Node(string i, Node* l, Node* r) {
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
+
+
+
+int main()
+{
+	
+}
